87893d3f07125e14a9a018c4a6c7278f7a219794
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2025-10-03 15:54:32 +0700

    style: reorder class names for consistency in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { HydrateClient } from "~/trpc/server";
 export default async function Home() {
 	return (
 		<HydrateClient>
-			<main className="p-8 grid grid-cols-2 gap-4">
+			<main className="grid grid-cols-2 gap-4 p-8">
 				<TeamTable />
 				<TeamTable />
 				<div className="col-span-2">
