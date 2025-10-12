3fdebaac83842b75f24e7afdab17e0b6f69116f3
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2025-10-12 17:36:00 +0700

    feat: remove nitroV2 plugin from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,9 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import netlify from '@netlify/vite-plugin-tanstack-start'
-import {nitroV2Plugin} from '@tanstack/nitro-v2-vite-plugin'
 
 const config = defineConfig({
   plugins: [
-    nitroV2Plugin(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
